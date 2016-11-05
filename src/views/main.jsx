8fa4e1ff8acb29e66ad801ddc8ada29d71e0869b
lincenying <lincenying@qq.com>
2016-11-06 01:17:31 +0800

    记录滚动条

--- src/views/main.jsx
+++ src/views/main.jsx
@@ -1,37 +1,55 @@
-import React from 'react'
+import React, {Component, PropTypes} from 'react'
 import {connect} from 'dva'
+import ls from 'store2'
+import {propTypes} from '../decorators'
 import MainItem from "../components/main-item.jsx"
 import Footer from '../components/footer.jsx'
 
-const Main = ({dispatch, location: { pathname }, topics: {list, hasNext, curpage}}) => {
-    const handleLoadMore = () => {
-        dispatch({type: 'topics/query', payload: { page: curpage + 1, pathname }})
+function mapStateToProps(state) {
+    return {
+        posts: state.topics
     }
-    const lists = list.map(list => {
+}
+
+@connect(mapStateToProps)
+@propTypes({
+    posts: PropTypes.object
+})
+export default class Main extends Component {
+    constructor(props) {
+        super(props)
+        this.handleLoadMore = this.handleLoadMore.bind(this)
+    }
+    componentDidMount() {
+        const path = this.props.location.pathname
+        const scrollTop = ls.get(path) || 0
+        window.scrollTo(0, scrollTop)
+    }
+    handleLoadMore() {
+        const {curpage} = this.props.posts
+        const {pathname} = this.props.location
+        this.props.dispatch({type: 'topics/query', payload: { page: curpage + 1, pathname }})
+    }
+    render() {
+        const {list, hasNext} = this.props.posts
+        const lists = list.map(list => {
+            return (
+                <MainItem key={list._id} list={list} />
+            )
+        })
+        const loadMore = hasNext ? <a onClick={this.handleLoadMore} href="javascript:;">加载更多</a> : <span>好厉害, 竟然翻到最后一页了...</span>
         return (
-            <MainItem key={list._id} list={list} />
+            <div className="g-mn">
+                <div className="posts">
+                    {lists}
+                </div>
+                <div className="box m-page box-do">
+                    <div className="w-icon w-icon-2" />
+                    <div className="w-icon w-icon-3" />
+                    {loadMore}
+                </div>
+                <Footer />
+            </div>
         )
-    })
-    const loadMore = hasNext ? <a onClick={handleLoadMore} href="javascript:;">加载更多</a> : <span>好厉害, 竟然翻到最后一页了...</span>
-    return (
-        <div className="g-mn">
-            <div className="posts">
-                {lists}
-            </div>
-            <div className="box m-page box-do">
-                <div className="w-icon w-icon-2" />
-                <div className="w-icon w-icon-3" />
-                {loadMore}
-            </div>
-            <Footer />
-        </div>
-    )
+    }
 }
-
-Main.propTypes = {}
-
-function mapStateToProps({ topics }) {
-    return { topics }
-}
-
-export default connect(mapStateToProps)(Main)
