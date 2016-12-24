1d7a3a3fdcc01716ab2073976ababdb2604e28b0
lincenying <lincenying@qq.com>
2016-12-24 23:59:41 +0800

    更新cooking配置文件

--- cooking.conf.js
+++ cooking.conf.js
@@ -21,7 +21,6 @@ var config = {
     devServer: {
         port: 8080,
         publicPath: '/',
-        clean: false,
         proxy: {
             '/api/**': {
                 target: 'http://localhost:3000/',
